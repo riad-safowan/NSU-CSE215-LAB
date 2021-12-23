d109293a1c58c450e1027bf3fc7955560bc62dbf
Riad Safowan <riad.safowan@northsouth.edu>
2021-12-23 13:57:17 +0600

    Lab 13 task 1

--- src/com/riadsafowan/java/Lab13.java
+++ src/com/riadsafowan/java/Lab13.java
@@ -3,26 +3,23 @@ package com.riadsafowan.java;
 import java.util.Scanner;
 
 public class Lab13 {
-    public static void main(String[] args) {
-
-        int[] n = new int[10];
-        try {
+    static class Task1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int[] array = new int[10];
+            System.out.print("Input 10 positive integers: ");
             for (int i = 0; i < 10; i++) {
-                n[i] = takePositiveInt();
+                try {
+                    int inputNum = scanner.nextInt();
+                    if (inputNum < 0)
+                        throw new Exception();
+                    array[i] = inputNum;
+                } catch (Exception e) {
+                    System.out.println("Input positive integer only !");
+                    i--;
+                }
             }
-        } catch (Exception e) {
-            System.out.println("Entered a negative number");
+            for (int i : array) System.out.print(i + " ");
         }
-
-    }
-
-    public static int takePositiveInt() throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter positive number");
-        int n = scanner.nextInt();
-        if (n < 0) {
-            throw new Exception();
-        }
-        return n;
     }
 }
